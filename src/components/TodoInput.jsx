040aa20eaedd951d0fe7c9adfb0b44cf7780f6ee
Abisheik-Raj <dgodly2004@gmail.com>
2024-07-28 15:09:31 +0530

    added keystroke sounds

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,8 +3,14 @@ import { useState } from "react"
 export default function TodoInput({buttonType,setButtonType ,todoValue,setTodoValue,handleTodoUpdate}) {
 
     return <div>
+        <audio id="mySound" src="public\255389-PA4000-Keyboard-Enter-Key-10.wav"></audio>
           <header>
             <input onChange={(event)  => {
+              const audio = document.getElementById("mySound");
+              if (audio) {
+                audio.currentTime = 0; 
+                audio.play();
+              }
               console.log(event.target.value)
             setTodoValue(event.target.value); 
             } 
